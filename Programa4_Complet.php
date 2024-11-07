8337b0c2a814f8a524844727c8ae61211e76c5dd
Kounzil <alejandromesisnas3@gmail.com>
2024-11-07 09:56:49 -0600

    Add files via upload

---
+++ Programa4_Complet.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montez&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Montez&display=swap" rel="stylesheet">
+    <title>Document</title>
+    <link rel = "stylesheet" href = "Programa4.css">
+    <style>
+
+.contenedor{
+    overflow-x: hidden
+}
+
+.busqueda2{
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+}
+
+.busqueda2 h3{
+        font-family: Josefin sans;
+        background-color: rgba(255, 255, 255, .6);
+        border-radius: 5px;
+        font-size: 20px;
+        width: 178px;
+        height: 40px;
+    
+}
+    </style>
+</head>
+<body>
+    <div class="contenedor">
+    <header>
+        <h1 class="title">ÍNDICE DE PROGRAMAS: UNIDAD 2</h1>
+        <h1 class="title">C.B.Ti.s No.107</h1>
+    </header>
+        
+      
+            <div class="busqueda">
+                        <h3>Insertar Registro</h3>
+                        <a href = "Programa1_Insert/formulario_registro.php"><img src = "https://cdn-icons-png.freepik.com/512/8463/8463769.png" alt = "Insertar" class="foto1"></a>
+                        <video width="340px" src="Video_Programacion/Video_Programa1.mp4" controls>Video Programa No.1</video>>
+                        <h3>Eliminar Registro</h3>
+                        <a href = "Programa2_Elim/formulario_eliminar.php"><img src = "https://cdn-icons-png.flaticon.com/512/5126/5126069.png" alt = "Eliminar" id="foto2"></a>
+                        <video width="340px" src="Video_Programacion/Video_Programa2.mp4" controls>Video Programa No.2</video>
+                            
+             </div>    
+
+            <div class="busqueda2">
+
+                        <h3>Actualizar Registro</h3>
+                        <a href = "Programa3_Actu/Form_Busqueda.php"><img src = "https://cdn-icons-png.flaticon.com/512/2581/2581896.png" alt = "Eliminar" id="foto3"></a>
+                        <video width="340px" src="Video_Programacion/Video_Programa3.mp4" controls>Video Programa No.2</video>
+                        <h3>Programa 4 general</h3>
+                        <video width="340px" src="Video_Programacion/Video_Programa4.mp4" controls>Video Programa No.2</video>
+
+                        </div>    
+            
+             <header>
+                    <h1>5° APR-M Programación</h1>
+                    <h1>Desarrollado por Los Pou Corporation ®</h1>
+                    <h1>Esperemos y les guste nuestro proyecto de Unidad</h1>
+                </header>
+
+          
+
+    </div>
+</body>
+</html>
